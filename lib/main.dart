1a4ef8a76304fad8782b4b52bf40008437dfcd6b
reika178 <rechiko178@gmail.com>
2020-08-01 21:29:35 +0900

    finish!
    firebase機能の追加できた

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ Widget _buildBody(BuildContext context) {
        child: ListTile(
          title: Text(record.name),
          trailing: Text(record.votes.toString()),
-         onTap: () => record.reference.updateData({'votes': record.votes + 1}),
+         onTap: () => record.reference.updateData({'votes': FieldValue.increment(1)}),
        ),
      ),
    );
